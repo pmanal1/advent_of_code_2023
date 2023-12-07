db058f747bdc052b22f2bf1ec7fb8f3b46f429cb
Paulo Manalo <paulomanalo792@gmail.com>
2023-12-06 23:02:03 -0500

    Day 4 part 2

--- day4.py
+++ day4.py
@@ -1,8 +1,13 @@
-# Each line is a card, the set of numbers to the left are the winning numbers, the set of numbers to the right are your numbers
-# For each card:
-#   Check how many matches there are
-#   Count up the points (exponent 2)
-#   Add it to a sum
+# Day 4 Part 2
+# start with card 1, count the number of matches
+# the number of matches corresponds to the number of copies of the next n cards
+# make a queue "array" corresponding to the copies of the next n cards
+# add one to the total amount of cards
+# pop the first index of the queue
+# on the next iteration, perform step 1 queue[0] + 1 times
+# add queue[0] + 1 to the total amount of cards
+
+# instead of performing step 1 queue[0] + 1 times, add queue[0] + 1 when adding the copies of cards
 
 # removes empty spaces in list
 def filterList(x):
@@ -12,7 +17,8 @@ def filterList(x):
 
 file = open('input.txt').readlines()
 
-totalPoints = 0
+numCards = 0
+queue = [0]
 for lineIndex, line in enumerate(file):
     line = line.split(": ")[1] # Remove Game X:
     
@@ -22,20 +28,24 @@ for lineIndex, line in enumerate(file):
     winningNumbers = filterList(winningNumbers)
     myNumbers = filterList(myNumbers)
 
-    print(winningNumbers)
-    print(myNumbers)
-
-    currentCardPoints = 0
+    if(len(queue) == 0): # just in case the queue is empty, the for loop needs to continue
+        queue.append(0)
+    
+    copiesToMake = 0
     for myNumber in myNumbers:
         if(myNumber in winningNumbers):
-            if(currentCardPoints == 0):
-                print("start", myNumber)
-                currentCardPoints += 1
-            else:
-                print("yay", myNumber)
-                currentCardPoints *= 2
+            copiesToMake += 1
     
-    print("total", currentCardPoints)
-    totalPoints += currentCardPoints
-
-print(totalPoints)
+    index = 1
+    while(copiesToMake > 0): # Go through the queue and add the copies
+        if(len(queue) - 1 < index): # indicates the first copy of a game, need to append
+            queue.append(queue[0] + 1) 
+        else:
+            queue[index] += queue[0] + 1 # queue[0] + 1 because that's how many copies of the current card we have
+        
+        copiesToMake -= 1
+        index += 1
+    
+    numCards += queue.pop(0) + 1
+            
+print(numCards)
